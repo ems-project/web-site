8630dca0e94a73e97ce11ab2579c9f1d4b8999a1
theus deka <me@theus.be>
2018-01-14 09:14:37 +0100

    Integrated with the client helper

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
 	/**
-	 * @Route("/")
+	 * @Route("/", name="homepage")
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
     public function homepage()
